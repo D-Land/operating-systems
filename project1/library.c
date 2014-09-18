afb2562cbd764f15a6d887baec27381306c96b2a
Drew Land <dland@thinkthroughmath.com>
2014-09-18 11:47:46 -0400

    Add some additional functions -- Compiling state

--- project1/library.c
+++ project1/library.c
@@ -6,37 +6,24 @@
 #include <fcntl.h>
 #include <sgtty.h>
 #include <termios.h>
+#include <time.h>
 
 #include <stdio.h>
 
 typedef unsigned short color_t;
-struct termios original;
+struct termios original_terminal_settings;
+unsigned int map_size;
+char *addr;
+int fd;
 
-void init_graphics(){
-  int fd;
-  unsigned int length;
-  char *addr;
-  char *pathname = "/dev/fb0";
-
-  struct fb_var_screeninfo *res_info;
-  struct fb_fix_screeninfo *bit_depth;
-
-  fd = open(pathname, O_RDWR);
-
-  ioctl(fd, FBIOGET_VSCREENINFO, res_info);
-  ioctl(fd, FBIOGET_FSCREENINFO, bit_depth);
-
-  length = res_info->yres_virtual * bit_depth->line_length;
-
-  addr = mmap(NULL, length, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
-}
+// - Echo Enable/Disable -------------------------------------------------------
 
 void disable_echo(){
   struct termios disabled;
 
-  ioctl(0, TCGETS, &original);
+  ioctl(0, TCGETS, &original_terminal_settings);
 
-  disabled = original;
+  disabled = original_terminal_settings;
   disabled.c_lflag &= ~ECHO;
   disabled.c_lflag &= ~ICANON;
 
@@ -44,22 +31,62 @@ void disable_echo(){
 }
 
 void enable_echo(){
-  ioctl(0, TCSETS, &original);
+  ioctl(0, TCSETS, &original_terminal_settings);
 }
 
-int main(int argc, char* argv[]){
-  char password[500];
+void init_graphics(){
+  char *pathname = "/dev/fb0";
 
-  /* init_graphics(); */
+  struct fb_var_screeninfo *virtual_resolution_info;
+  struct fb_fix_screeninfo *bit_depth_info;
+
+  fd = open(pathname, O_RDWR);
+
+  ioctl(fd, FBIOGET_VSCREENINFO, virtual_resolution_info);
+  ioctl(fd, FBIOGET_FSCREENINFO, bit_depth_info);
+
+  map_size = virtual_resolution_info->yres_virtual * bit_depth_info->line_length;
+
+  addr = mmap(NULL, map_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+
   disable_echo();
+}
 
-  printf("\n Enter your password: \n");
-  scanf("%s", &password);
-  printf("\nYour Password: %s\n", password);
-
+void exit_graphics(){
+  munmap(addr, map_size);
+  close(fd);
   enable_echo();
+}
 
-  printf("\n Enter your password: \n");
-  scanf("%s", &password);
-  printf("\nYour Password: %s\n", password);
+void clear_screen(){
+  printf("\033[2J");
+}
+
+void sleep_ms(int t){
+  struct timespec sleep_settings, other;
+
+  sleep_settings.tv_sec  = 0;
+  sleep_settings.tv_nsec = t * 1000000;
+
+  nanosleep(&sleep_settings, &other);
+}
+
+int get_key(){
+  struct timeval timeout;
+  timeout.tv_sec = 0;
+  timeout.tv_usec = 0;
+
+  fd_set fds;
+  FD_ZERO(&fds);
+  FD_SET(0, &fds);
+  select(1, &fds, NULL, NULL, &timeout);
+  return FD_ISSET(0, &fds);
+}
+
+// - Temp ----------------------------------------------------------------------
+
+int main(int argc, char* argv[]){
+
+  init_graphics();
+  enable_echo();
 }
