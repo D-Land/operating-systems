0e7684c42ae06577dee5174ed75e5dc5543860ae
Drew Land <dland@thinkthroughmath.com>
2014-09-19 15:50:58 -0400

    Update

--- project1/library.c
+++ project1/library.c
@@ -107,8 +107,9 @@ int main(int argc, char* argv[]){
 
   init_graphics();
   clear_screen();
-  for(x = 0; x < 200){
+  while(x < 200){
     draw_pixel(x, 0, c);
+    x = x+1;
   }
   while(1){
     if(getkey())
