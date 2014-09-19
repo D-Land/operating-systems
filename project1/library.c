1e141187d19647fd508705302044c0ff3096a1ef
Drew Land <dland@thinkthroughmath.com>
2014-09-19 15:55:42 -0400

    Map_size fixed

--- project1/library.c
+++ project1/library.c
@@ -108,12 +108,14 @@ int main(int argc, char* argv[]){
   init_graphics();
   clear_screen();
   clear_screen();
+  clear_screen();
+  clear_screen();
+  clear_screen();
   while(x < 200){
     draw_pixel(x, 0, c);
     x = x+1;
   }
   while(1){
-  clear_screen();
     if(getkey())
       break;
   }
