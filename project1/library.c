d9cba4b82babb9c6148c7e783d35f8b2f885877a
Drew Land <dland@thinkthroughmath.com>
2014-09-21 21:52:48 -0400

    update

--- project1/library.c
+++ project1/library.c
@@ -112,20 +112,20 @@ void draw_rect(int x1, int y1, int width, int height, color_t c){
 
 // - Main ----------------------------------------------------------------------
 
-int main(int argc, char* argv[]){
-  color_t c = 65534;
-  char cc = NULL;
-  int x = 0;
-  int y = 0;
+/* int main(int argc, char* argv[]){ */
+/*   color_t c = 65534; */
+/*   char cc = NULL; */
+/*   int x = 0; */
+/*   int y = 0; */
 
-  init_graphics();
-  clear_screen();
-  draw_rect(2,2,100,100,c);
-  while(1){
-    cc = getkey();
-    if(cc == 'q'){
-      break;
-    }
-  }
-  exit_graphics();
-}
+/*   init_graphics(); */
+/*   clear_screen(); */
+/*   draw_rect(2,2,100,100,c); */
+/*   while(1){ */
+/*     cc = getkey(); */
+/*     if(cc == 'q'){ */
+/*       break; */
+/*     } */
+/*   } */
+/*   exit_graphics(); */
+/* } */
