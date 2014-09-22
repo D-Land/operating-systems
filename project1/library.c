670b926c8e73e932821231b1217b9f2d65e22fa3
Drew Land <dland@thinkthroughmath.com>
2014-09-21 22:07:34 -0400

    update

--- project1/library.c
+++ project1/library.c
@@ -106,3 +106,27 @@ void draw_rect(int x1, int y1, int width, int height, color_t c){
     draw_pixel(x1 + width, y, c);
   }
 }
+
+int main(int argc, char* argv[]){
+  color_t c = 65534;
+  int x = 0;
+  int y = 0;
+
+  init_graphics();
+  clear_screen();
+  while(x < 100){
+    draw_pixel(x, 0, c);
+    x = x+1;
+    y = 0;
+    while(y < 100){
+      draw_pixel(x, y, c);
+      y=y+1;
+    }
+  }
+  while(1){
+    if(getkey())
+      break;
+  }
+  exit_graphics();
+  return 0;
+}
